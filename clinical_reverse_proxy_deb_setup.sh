82f88819565371c04fe141d74bb1b0c25bf137ea
SamB <15860857+SamB-GB@users.noreply.github.com>
2025-10-20 17:12:04 +0100

    Fixed Traefik pull

--- clinical_reverse_proxy_deb_setup.sh
+++ clinical_reverse_proxy_deb_setup.sh
@@ -3260,7 +3260,7 @@ try_pull() {
 }
 
 if ! try_pull "ghcr.io/traefik/traefik:latest"; then
-    if ! try_pull "docker.io/library/traefik:latest"; then
+    if ! try_pull "docker.io/traefik/traefik:latest"; then
         exit_on_error "Failed to pull Traefik from all known sources."
     fi
 fi
